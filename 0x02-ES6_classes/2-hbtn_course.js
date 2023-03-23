d6ec61571419691764840b00404e87b426af1d58
Cayvoh-K <muhiakevin12@gmail.com>
2023-03-23 21:03:15 +0000

    getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number');
-      throw new TypeError('length must be a number');
+      throw new TypeError('Length must be a number');
     } else if (!Array.isArrray(students)) {
-      throw new TypeError('students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
 
     this._name = name;
@@ -27,21 +27,21 @@ export default class HolbertonCourse {
 
   set name(name) {
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new TypeError('length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
   set students(students) {
     if (!Array.isArrray(students)) {
-      throw new TypeError('students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
   }  
