6a57478bdeadf1d921cc160912a316abe78ed093
Cayvoh-K <muhiakevin12@gmail.com>
2023-03-27 21:03:00 +0000

    map and data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const items = new Map();
+  items.set('Apples', 10);
+  items.set('Tomatoes', 10);
+  items.set('Pasta', 10);
+  items.set('Rice', 10);
+  items.set('Banana', 5);
+
+  return items;
+}
